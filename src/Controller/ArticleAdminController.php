8fea110ceba7a225627b0fb13dbbeac7163fc5be
Alexandru Pietrareanu <alexandru.pietrareanu@gmail.com>
2021-11-20 16:52:15 +0200

    upload

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleFormType;
 use App\Repository\ArticleRepository;
+use App\Service\UploaderHelper;
 use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Sluggable\Util\Urlizer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,7 +21,7 @@ class ArticleAdminController extends BaseController
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em, Request $request)
+    public function new(EntityManagerInterface $em, Request $request, UploaderHelper $uploaderHelper)
     {
         $form = $this->createForm(ArticleFormType::class);
 
@@ -26,6 +29,13 @@ class ArticleAdminController extends BaseController
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var Article $article */
             $article = $form->getData();
+
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $form['imageFile']->getData();
+            if($uploadedFile) {
+                $newFilename = $uploaderHelper->uploadArticleImage($uploadedFile);
+                $article->setImageFilename($newFilename);
+            }
 
             $em->persist($article);
             $em->flush();
@@ -44,7 +54,7 @@ class ArticleAdminController extends BaseController
      * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article, Request $request, EntityManagerInterface $em)
+    public function edit(Article $article, Request $request, EntityManagerInterface $em, UploaderHelper $uploaderHelper)
     {
         $form = $this->createForm(ArticleFormType::class, $article, [
             'include_published_at' => true
@@ -52,6 +62,14 @@ class ArticleAdminController extends BaseController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var UploadedFile $uploadedFile */
+            $uploadedFile = $form['imageFile']->getData();
+            if($uploadedFile) {
+                $newFilename = $uploaderHelper->uploadArticleImage($uploadedFile);
+                $article->setImageFilename($newFilename);
+            }
+
             $em->persist($article);
             $em->flush();
 
@@ -65,13 +83,6 @@ class ArticleAdminController extends BaseController
         return $this->render('article_admin/edit.html.twig', [
             'articleForm' => $form->createView()
         ]);
-    }
-
-    /**
-     * @Route("admin/uplpad/test", name="upload_test")
-     */
-    public function temporaryUploadAction(Request $request){
-        dd($request->files->get('image'));
     }
 
     /**
---
+++ src/Service/UploaderHelper.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use Gedmo\Sluggable\Util\Urlizer;
+use Symfony\Component\Asset\Context\RequestStackContext;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class UploaderHelper
+{
+    const ARTICLE_IMAGE = 'article_image';
+    /**
+     * @var string
+     */
+    private $uploadsPath;
+    /**
+     * @var RequestStackContext
+     */
+    private $requestStackContext;
+
+    public function __construct(string $uploadsPath, RequestStackContext $requestStackContext){
+
+        $this->uploadsPath = $uploadsPath;
+        $this->requestStackContext = $requestStackContext;
+    }
+
+    public function uploadArticleImage(UploadedFile $uploadedFile): string
+    {
+        $destination = $this->uploadsPath.'/'.self::ARTICLE_IMAGE;
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = Urlizer::urlize($originalFilename).uniqid().'-'.$uploadedFile->guessExtension();
+
+        $uploadedFile->move(
+            $destination,
+            $newFilename
+        );
+
+        return $newFilename;
+    }
+
+    public function getPublicPath(string $path): string
+    {
+        return  $this->requestStackContext->getBasePath().'/uploads/'.$path;
+    }
+}
